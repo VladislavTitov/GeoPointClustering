90d7f42c98ff66045d1b89e1cd41356cdc64127c
VladislavTitov <titovvlad1997@yandex.ru>
2018-03-04 02:02:48 +0300

    color points

--- test_gmplot.py
+++ test_gmplot.py
@@ -29,5 +29,19 @@ temp = np.hsplit(db.cluster_centers_, 2)
 cntr_lat = temp[0].reshape(1, -1)[0]
 cntr_lng = temp[1].reshape(1, -1)[0]
 
-gmap2.scatter(cntr_lat, cntr_lng, 'red', marker=True)
-gmap2.draw("mymap_cntr.html")
+gmap2.scatter(cntr_lat, cntr_lng, 'black', marker=True)
+
+labels = db.labels_
+unique_labels = set(labels)
+n_clusters_ = len(unique_labels) - (1 if -1 in labels else 0)
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
+colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_labels)))
+for k, col in zip(unique_labels, colors):
+    labeled_points = latlng[labels == k]
+    split_array = np.hsplit(labeled_points, 2)
+    plt.scatter(split_array[0].reshape(1, -1)[0], split_array[1].reshape(1, -1)[0], c=col, marker=True)
+
+plt.show()
+#gmap2.draw("mymap_cntr.html")
